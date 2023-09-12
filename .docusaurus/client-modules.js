92b5b798417d26b7beda641719e63ee7cecaa4e7
masaleiro <mariosaleiro@gmail.com>
2023-09-12 21:53:51 +0100

    Added base docusaurus installation

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require('/home/masaleiro/Code/UAlg/docusaurus-ghpages/node_modules/infima/dist/css/default/default.css'),
+  require('/home/masaleiro/Code/UAlg/docusaurus-ghpages/node_modules/@docusaurus/theme-classic/lib/prism-include-languages'),
+  require('/home/masaleiro/Code/UAlg/docusaurus-ghpages/node_modules/@docusaurus/theme-classic/lib/nprogress'),
+  require('/home/masaleiro/Code/UAlg/docusaurus-ghpages/src/css/custom.css'),
+];
